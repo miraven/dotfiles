af40301a3a577e0d1fc2c2a38e603470c7b8fb98
Raven Nuega <nuega12@gmail.com>
2025-06-08 19:33:46 -0400

    harpoon stuff, might delete later

--- .config/nvim/lua/miraven/plugin_configs/harpoon.lua
+++ .config/nvim/lua/miraven/plugin_configs/harpoon.lua
@@ -7,7 +7,7 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>x", function() harpoon:list():remove() end)
 
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-t>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-y>", function() harpoon:list():select(2) end)
@@ -15,5 +15,5 @@ vim.keymap.set("n", "<C-g>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
+-- vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
+--vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
